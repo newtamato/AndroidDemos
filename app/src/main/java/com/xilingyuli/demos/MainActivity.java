2f163207f05cfbf2ad1e07fd5d0af69c3e60f449
xilingyuli <786979248@qq.com>
2017-05-21 19:45:47 +0800

    拖拽效果

--- app/src/main/java/com/xilingyuli/demos/MainActivity.java
+++ app/src/main/java/com/xilingyuli/demos/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.xilingyuli.demos.animation.DragActivity;
 import com.xilingyuli.demos.screenshot.ScreenShotActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, ScreenShotActivity.class);
+        //Intent intent = new Intent(this, ScreenShotActivity.class);
+        Intent intent = new Intent(this, DragActivity.class);
         startActivity(intent);
         finish();
     }
